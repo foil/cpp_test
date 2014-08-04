f11e4f827203e590a47b3ee68413e91e2cf9d540
Matt Zhang <matt.zhang193@gmail.com>
2014-08-05 09:35:56 +1000

    Initial Commit

---
+++ CppTest/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  CppTest
+//
+//  Created by meng zhang on 5/08/2014.
+//  Copyright (c) 2014 meng zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+	// insert code here...
+	std::cout << "Hello, World!\n";
+    return 0;
+}
+
